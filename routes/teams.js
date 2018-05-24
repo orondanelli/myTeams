d519af754bf27cfccbf2af28848375be244a1619
orondanelli <orlando.rondanelli@gmail.com>
2018-05-24 12:33:17 -0400

    CRUD working

--- routes/teams.js
+++ routes/teams.js
@@ -1,17 +1,23 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
-var flash = require('express-flash');
 var session = require('express-session');
 
 router.get('/',function(req, res) {
    models.Level.findAll()
     .then(function(level) {
+      models.Team.findAll({})
+          .then(function(teams) {
+              res.render('teams',{arrayTeams: teams});
+          console.log(teams);
+          });
+      /*
         models.Team.findAll({include: [models.Level]})
             .then(function(teams) {
                 res.render('teams',{arrayTeams: teams,arrayLevels:level});
             console.log(teams);
             });
+          */
     });
 });
     //models.Team.findAll({include: [models.Level]})
@@ -20,8 +26,8 @@ router.get('/',function(req, res) {
 router.post('/create', function(req, res, next) {
   models.Team.create({
     teamName: req.body.teamName,
-    currentLevelInd: 'X',
-    LevelId: req.body.selectLevelName
+    areaName: req.body.selectAreaName
+    //,LevelId: req.body.selectLevelName
   }).then(function() {
         console.log(req.body.selectLevelName);
     res.redirect('/equipos');
@@ -40,19 +46,33 @@ router.get('/:team_id/delete', function(req, res) {
   });
 });
 
+router.get('/:team_id/update', function(req, res) {
+  models.Team.findById(req.params.team_id)
+  .then(function(team) {
+    res.render('teams_edit',{arrayTeams: JSON.stringify(team)});
+    console.log(JSON.stringify(team));
+  });
+});
+
 router.put('/:team_id/update', function (req, res, next) {
   console.log('entro');
+  console.log(req.params.team_id);
+  console.log(req.body.teamName);
  models.Team.update({
    teamName: req.body.teamName,
-   currentLevelInd: 'X',
-   LevelId: req.body.selectLevelName},
-   {where: req.params.team_id}
+   teamArea: req.body.selectAreaName},
+   {where: {id: req.params.team_id}}
  )
  .then(function(rowsUpdated) {
+   console.log(rowsUpdated);
    res.redirect('/equipos');
-   console.log(req.params.team_id)
+   console.log(req.params.team_id);
+
  })
- .catch(next)
+ .catch(function (err){
+    console.log(err);
+ }
+)
 })
 
 module.exports = router;
