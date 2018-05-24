906e48b1a8be3b54c1668bcf13acad1041ff621e
orondanelli <orlando.rondanelli@gmail.com>
2018-05-24 12:38:26 -0400

    CRUD working

--- routes/teams.js
+++ routes/teams.js
@@ -1,78 +1,73 @@
-var models  = require('../models');
-var express = require('express');
-var router  = express.Router();
-var session = require('express-session');
+const models  = require('../models');
+const express = require('express');
+const router  = express.Router();
 
 router.get('/',function(req, res) {
-   models.Level.findAll()
-    .then(function(level) {
-      models.Team.findAll({})
-          .then(function(teams) {
-              res.render('teams',{arrayTeams: teams});
-          console.log(teams);
-          });
-      /*
-        models.Team.findAll({include: [models.Level]})
-            .then(function(teams) {
-                res.render('teams',{arrayTeams: teams,arrayLevels:level});
-            console.log(teams);
-            });
-          */
-    });
+    models.Level.findAll()
+        .then(function() {
+            models.Team.findAll({})
+                .then(function(teams) {
+                    res.render('teams',{arrayTeams: teams});
+                    console.log(teams);
+                });
+            /*
+              models.Team.findAll({include: [models.Level]})
+                  .then(function(teams) {
+                      res.render('teams',{arrayTeams: teams,arrayLevels:level});
+                  console.log(teams);
+                  });
+                */
+        });
 });
-    //models.Team.findAll({include: [models.Level]})
+//models.Team.findAll({include: [models.Level]})
 
-
-router.post('/create', function(req, res, next) {
-  models.Team.create({
-    teamName: req.body.teamName,
-    areaName: req.body.selectAreaName
-    //,LevelId: req.body.selectLevelName
-  }).then(function() {
+router.post('/create', function(req, res) {
+    models.Team.create({
+        teamName: req.body.teamName,
+        areaName: req.body.selectAreaName
+        //,LevelId: req.body.selectLevelName
+    }).then(function() {
         console.log(req.body.selectLevelName);
-    res.redirect('/equipos');
-    //res.render('teams',{exito:1, message: req.flash('success_messages')});
-
-  });
+        res.redirect('/equipos');
+    });
 });
 
 router.get('/:team_id/delete', function(req, res) {
-  models.Team.destroy({
-    where: {
-      id: req.params.team_id
-    }
-  }).then(function() {
-    res.redirect('/equipos');
-  });
+    models.Team.destroy({
+        where: {
+            id: req.params.team_id
+        }
+    }).then(function() {
+        res.redirect('/equipos');
+    });
 });
 
 router.get('/:team_id/update', function(req, res) {
-  models.Team.findById(req.params.team_id)
-  .then(function(team) {
-    res.render('teams_edit',{arrayTeams: JSON.stringify(team)});
-    console.log(JSON.stringify(team));
-  });
+    models.Team.findById(req.params.team_id)
+        .then(function(team) {
+            res.render('teams_edit',{arrayTeams: JSON.stringify(team)});
+            console.log(JSON.stringify(team));
+        });
 });
 
-router.put('/:team_id/update', function (req, res, next) {
-  console.log('entro');
-  console.log(req.params.team_id);
-  console.log(req.body.teamName);
- models.Team.update({
-   teamName: req.body.teamName,
-   teamArea: req.body.selectAreaName},
-   {where: {id: req.params.team_id}}
- )
- .then(function(rowsUpdated) {
-   console.log(rowsUpdated);
-   res.redirect('/equipos');
-   console.log(req.params.team_id);
+router.put('/:team_id/update', function (req, res) {
+    console.log(req.params.team_id);
+    console.log(req.body.teamName);
+    models.Team.update({
+            teamName: req.body.teamName,
+            teamArea: req.body.selectAreaName},
+        {where: {id: req.params.team_id}}
+    )
+        .then(function(rowsUpdated) {
+            console.log(rowsUpdated);
+            res.redirect('/equipos');
+            console.log(req.params.team_id);
 
- })
- .catch(function (err){
-    console.log(err);
- }
-)
+        })
+        .catch(function (err){
+                console.log(err);
+            }
+        )
 })
 
 module.exports = router;
